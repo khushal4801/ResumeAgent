36fcac7aaca46c567790400002ef2ac41eaa1dc7
khushal4801 <khushal.19bcon395@jecrcu.edu.in>
2026-08-09 11:07:13 +0530

    Pushing folder structure

---
+++ ResumeAgent/src/main/java/com/example/ResumeAgent/ResumeAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResumeAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAgentApplication.class, args);
+	}
+
+}
---
+++ ResumeAgent/src/test/java/com/example/ResumeAgent/ResumeAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ResumeAgent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
